52271247d1f344459f9b72a51cb6bebd5374942b
tonousa <tonousa@hotmail.com>
2015-08-12 19:02:29 -0400

    517

--- HelperMethods/Controllers/HomeController.cs
+++ HelperMethods/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using HelperMethods.Models;
 
 namespace HelperMethods.Controllers
 {
@@ -21,5 +22,15 @@ namespace HelperMethods.Controllers
             return View((object)message);
         }
 
+        public ActionResult CreatePerson()
+        {
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public ActionResult CreatePerson(Person person)
+        {
+            return View(person);
+        }
     }
 }
