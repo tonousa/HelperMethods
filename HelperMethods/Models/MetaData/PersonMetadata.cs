c0d964bfd2decafc29134b282dece960bef60ef7
tonousa <tonousa@hotmail.com>
2015-08-14 19:22:28 -0400

    560

--- HelperMethods/Models/MetaData/PersonMetadata.cs
+++ HelperMethods/Models/MetaData/PersonMetadata.cs
@@ -5,8 +5,26 @@ using System.Web.Mvc;
 
 namespace HelperMethods.Models
 {
+    [DisplayName("New Person")]
     public partial class PersonMetadata
     {
+        [HiddenInput(DisplayValue=false)]
+        public int PersonId { get; set; }
 
+        [Display(Name="First")]
+        public string FirstName { get; set; }
+
+        [Display(Name="Last")]
+        public string LastName { get; set; }
+
+        [Display(Name="Birth Date")]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
+
+        [Display(Name="Approved")]
+        public bool IsApproved { get; set; }
+
+        [UIHint("Enum")]
+        public Role Role { get; set; }
     }
 }
